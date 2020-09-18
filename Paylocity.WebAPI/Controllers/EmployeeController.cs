bbbfdbe0710c0f6b31450e6338a163d263a4b2cd
khuynh0101 <khuynh0101@gmail.com>
2020-09-17 22:02:25 -0400

    added webapi project

---
+++ Paylocity.WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using Paylocity.Data.Repositories;
+using Paylocity.Logging;
+using Paylocity.Service;
+using Paylocity.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Paylocity.WebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage CalculateBenefitsCost([FromBody] Employee employee)
+        {
+            try
+            {
+                EmployeeBenefitsService service = new EmployeeBenefitsService(new EmployeeBenefitsRepository());
+                BenefitsSummary summary = service.CalculateBenefitsCost(employee);
+                return Request.CreateResponse(HttpStatusCode.OK, new { summary });
+            }
+            catch (Exception ex)
+            {
+                Log.LogMessage(TracingLevel.ERROR, ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error");
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Save([FromBody] Employee employee)
+        {
+            try
+            {
+                EmployeeBenefitsService service = new EmployeeBenefitsService(new EmployeeBenefitsRepository());
+                //just in case website doesn't send the employee's benefits information
+                if (employee.BenefitsSummary == null)
+                    employee.BenefitsSummary = service.CalculateBenefitsCost(employee);
+                bool isSaved = service.Save(employee);
+                return Request.CreateResponse(HttpStatusCode.OK, new { isSaved });
+            }
+            catch (Exception ex)
+            {
+                Log.LogMessage(TracingLevel.ERROR, ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error");
+            }
+        }
+    }
+}
---
+++ Paylocity.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Paylocity.WebAPI.WebApiApplication" Language="C#" %>
