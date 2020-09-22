cad4ab0788edbda425dd7fe3aff987055a5ba676
khuynh0101 <khuynh0101@gmail.com>
2020-09-21 22:31:26 -0400

    added test for costpercheck value

--- Paylocity.EmployeeBenefitCalculatorTest/EmployeeBenefitsServiceTest.cs
+++ Paylocity.EmployeeBenefitCalculatorTest/EmployeeBenefitsServiceTest.cs
@@ -3,6 +3,7 @@ using Paylocity.Service;
 using Paylocity.Service.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 
 namespace Paylocity.EmployeeBenefitCalculatorTest
 {
@@ -13,73 +14,79 @@ namespace Paylocity.EmployeeBenefitCalculatorTest
         public void CalculateBenefitsCostForEmployeeWithoutLetterA_ShouldReturn1000()
         {
             Mock<Employee> mockEmployee = new Mock<Employee>();
-            mockEmployee.Object.Name = "test";
+            mockEmployee.Object.Name = "Khuong";
             BenefitsSummary benefitsSummary = new EmployeeBenefitsService(new EmployeeBenefitsRepository()).CalculateBenefitsCost(mockEmployee.Object);
 
             Assert.IsNotNull(benefitsSummary);
-            Assert.AreEqual(benefitsSummary.EmployeeCost, 1000, "Employee Cost incorrect");
-            Assert.AreEqual(benefitsSummary.TotalCost, 1000, "Employee Total Cost incorrect");
+            Assert.AreEqual(benefitsSummary.EmployeeCost, 1000.00m, "Employee Cost incorrect");
+            Assert.AreEqual(benefitsSummary.TotalCost, 1000.00m, "Employee Total Cost incorrect");
+            Assert.AreEqual(Math.Round(benefitsSummary.CostPerPayCheck, 2), 38.46m, "Cost per check is incorrect");
         }
 
         [TestMethod]
         public void CalculateBenefitsCostForEmployeeWithLetterA_ShouldReturn900()
         {
             Mock<Employee> mockEmployee = new Mock<Employee>();
-            mockEmployee.Object.Name = "atest";
+            mockEmployee.Object.Name = "Albert";
             BenefitsSummary benefitsSummary = new EmployeeBenefitsService(new EmployeeBenefitsRepository()).CalculateBenefitsCost(mockEmployee.Object);
 
-            Assert.AreEqual(benefitsSummary.EmployeeCost, 900, "Employee Cost incorrect");
-            Assert.AreEqual(benefitsSummary.TotalCost, 900, "Employee Total Cost incorrect");
+            Assert.AreEqual(benefitsSummary.EmployeeCost, 900.00m, "Employee Cost incorrect");
+            Assert.AreEqual(benefitsSummary.TotalCost, 900.00m, "Employee Total Cost incorrect");
+            Assert.AreEqual(Math.Round(benefitsSummary.CostPerPayCheck, 2), 34.62m, "Cost per check is incorrect");
         }
 
         [TestMethod]
         public void CalculateBenefitsCostForEmployeeAndDependentsWithoutLetterA_ShouldReturnDependentCostOf500_And_TotalCostOf1500()
         {
             Mock<Employee> mockEmployee = new Mock<Employee>();
-            mockEmployee.Object.Name = "test";
-            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "michelle" });
+            mockEmployee.Object.Name = "Khuong";
+            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "Michelle" });
             BenefitsSummary benefitsSummary = new EmployeeBenefitsService(new EmployeeBenefitsRepository()).CalculateBenefitsCost(mockEmployee.Object);
 
-            Assert.AreEqual(benefitsSummary.DependentsCost, 500, "Dependent Cost incorrect");
-            Assert.AreEqual(benefitsSummary.TotalCost, 1500, "Employee Total Cost incorrect");
+            Assert.AreEqual(benefitsSummary.DependentsCost, 500.00m, "Dependent Cost incorrect");
+            Assert.AreEqual(benefitsSummary.TotalCost, 1500.00m, "Employee Total Cost incorrect");
+            Assert.AreEqual(Math.Round(benefitsSummary.CostPerPayCheck, 2), 57.69m, "Cost per check is incorrect");
         }
 
         [TestMethod]
         public void CalculateBenefitsCostForEmployeeAndDependentsWithLetterA_ShouldReturnDependentCostOf450_And_TotalCostOf1450()
         {
             Mock<Employee> mockEmployee = new Mock<Employee>();
-            mockEmployee.Object.Name = "test";
-            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "amichelle" });
+            mockEmployee.Object.Name = "Khuong";
+            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "Adele" });
             BenefitsSummary benefitsSummary = new EmployeeBenefitsService(new EmployeeBenefitsRepository()).CalculateBenefitsCost(mockEmployee.Object);
 
-            Assert.AreEqual(benefitsSummary.DependentsCost, 450, "Dependent Cost incorrect");
-            Assert.AreEqual(benefitsSummary.TotalCost, 1450, "Employee Total Cost incorrect");
+            Assert.AreEqual(benefitsSummary.DependentsCost, 450.00m, "Dependent Cost incorrect");
+            Assert.AreEqual(benefitsSummary.TotalCost, 1450.00m, "Employee Total Cost incorrect");
+            Assert.AreEqual(Math.Round(benefitsSummary.CostPerPayCheck, 2), 55.77m, "Cost per check is incorrect");
         }
 
         [TestMethod]
         public void CalculateBenefitsCostForEmployeeAndTwoDependentsWithoutLetterA_ShouldReturnDependentCostOf1000_And_TotalCostOf2000()
         {
             Mock<Employee> mockEmployee = new Mock<Employee>();
-            mockEmployee.Object.Name = "test";
-            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "michelle" });
-            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "tom" });
+            mockEmployee.Object.Name = "Khuong";
+            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "Michelle" });
+            mockEmployee.Object.Dependents.Add(new Dependent() { Name = "Apple" });
             BenefitsSummary benefitsSummary = new EmployeeBenefitsService(new EmployeeBenefitsRepository()).CalculateBenefitsCost(mockEmployee.Object);
 
-            Assert.AreEqual(benefitsSummary.DependentsCost, 1000, "Dependent Cost incorrect");
-            Assert.AreEqual(benefitsSummary.TotalCost, 2000, "Employee Total Cost incorrect");
+            Assert.AreEqual(benefitsSummary.DependentsCost, 950.00m, "Dependent Cost incorrect");
+            Assert.AreEqual(benefitsSummary.TotalCost, 1950.00m, "Employee Total Cost incorrect");
+            Assert.AreEqual(Math.Round(benefitsSummary.CostPerPayCheck, 2), 75.00m, "Cost per check is incorrect");
         }
 
         [TestMethod]
         public void CalculateBenefitsCostForEmployeeAndTwoDependentsWithLetterA_ShouldReturnDependentCostOf900_And_TotalCostOf1800()
         {
             Mock<Employee> mockEmployee = new Mock<Employee>();
-            mockEmployee.Object.Name = "test";
+            mockEmployee.Object.Name = "Khuong";
             mockEmployee.Object.Dependents.Add(new Dependent() { Name = "Apple" });
             mockEmployee.Object.Dependents.Add(new Dependent() { Name = "Ariana" });
             BenefitsSummary benefitsSummary = new EmployeeBenefitsService(new EmployeeBenefitsRepository()).CalculateBenefitsCost(mockEmployee.Object);
 
-            Assert.AreEqual(benefitsSummary.DependentsCost, 900, "Dependent Cost incorrect");
-            Assert.AreEqual(benefitsSummary.TotalCost, 1900, "Employee Total Cost incorrect");
+            Assert.AreEqual(benefitsSummary.DependentsCost, 900.00m, "Dependent Cost incorrect");
+            Assert.AreEqual(benefitsSummary.TotalCost, 1900.00m, "Employee Total Cost incorrect");
+            Assert.AreEqual(Math.Round(benefitsSummary.CostPerPayCheck, 2), 73.08m, "Cost per check is incorrect");
         }
 
         [TestMethod]
@@ -107,9 +114,9 @@ namespace Paylocity.EmployeeBenefitCalculatorTest
                     return false;
                 foreach (var benefit in target.Benefits)
                 {
-                    if (benefit.DependentCost != 1000)
+                    if (benefit.DependentCost != 1000.00m)
                         return false;
-                    if (benefit.EmployeeCost != 1000)
+                    if (benefit.EmployeeCost != 1000.00m)
                         return false;
                 }
                 return true; 
