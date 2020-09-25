d12a15d2ac3cfb2155b186a623ff1f2d98fd9083
khuynh0101 <khuynh0101@gmail.com>
2020-09-25 08:15:35 -0400

    clean up repo

--- Paylocity.Data/Repositories/EmployeeBenefitsRepository.cs
+++ Paylocity.Data/Repositories/EmployeeBenefitsRepository.cs
@@ -2,13 +2,14 @@
 {
    public class EmployeeBenefitsRepository : IEmployeeBenefitsRepository
     {
+        private PaylocityEntities _context = new PaylocityEntities();
         public bool SaveEmployeeBenefits(Employee employeeDataModel)
         {
             int numRec = 0;
-            using (PaylocityEntities context = new PaylocityEntities())
-            { 
-                context.Employees.Add(employeeDataModel);
-                numRec = context.SaveChanges();
+            using (_context)
+            {
+                _context.Employees.Add(employeeDataModel);
+                numRec = _context.SaveChanges();
             }            
             return numRec > 0;
         }
