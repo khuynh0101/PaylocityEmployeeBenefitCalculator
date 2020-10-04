7abf0b78e7b673fa2804a9b273e4ad438c7fbd2f
khuynh0101 <khuynh0101@gmail.com>
2020-10-04 18:32:44 -0400

    remove constants

--- Paylocity.Common/Constants.cs
+++ Paylocity.Common/Constants.cs
@@ -8,7 +8,5 @@
         public const decimal DiscountPercentAmount = 10.00m;
         public const decimal DependentBenefitsCost = 500.00m;
         public const decimal EmployeeBenefitsCost = 1000.00m;
-
-        public const string C = "C";
     }
 }
